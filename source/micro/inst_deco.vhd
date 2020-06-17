0098803410a088e3aee9331a31fb492ee938c8ac
Ramses <carrocobra2@hotmail.com>
2020-06-16 22:42:25 -0500

    inst_deco_mods

--- source/micro/inst_deco.vhd
+++ source/micro/inst_deco.vhd
@@ -5,10 +5,7 @@ entity inst_deco is
     Port ( inst : in  STD_LOGIC_VECTOR (15 downto 0);
            A : out  STD_LOGIC_VECTOR (4 downto 0);
            B : out  STD_LOGIC_VECTOR (4 downto 0);
-			  reg_sel : out STD_LOGIC;
-           unit_sel : out  STD_LOGIC;
-           op_sel : out  STD_LOGIC_VECTOR (1 downto 0);
-           ci : out  STD_LOGIC;
+           inst_out : out  STD_LOGIC_VECTOR (4 downto 0);
            PC : out  STD_LOGIC_VECTOR (4 downto 0));
 end inst_deco;
 
@@ -18,10 +15,7 @@ begin
 	process(inst)
 		begin
 		if(inst(15) = '0') then
-			reg_sel <= inst(14);
-			unit_sel <= inst(13);
-			op_sel <= inst(12 downto 11);
-			ci <= inst(10);
+			inst_out <= inst(14 downto 10);
 			B <= inst(9 downto 5);
 			A <= inst(4 downto 0);
 		else
