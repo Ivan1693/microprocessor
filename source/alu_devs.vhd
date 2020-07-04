c4f13f85c382e7d5e9bedcd023249ada4e94a232
Zaiks13 <etszaiks@gmail.com>
2020-07-04 15:26:15 -0500

    Added shift operations 16-19

--- source/alu_devs.vhd
+++ source/alu_devs.vhd
@@ -5,7 +5,7 @@ package alu_devs is
 	component alu_16op is
 	port(
 		 unit_sel : in std_logic;
-		 op_sel : in std_logic_vector(1 downto 0);
+		 op_sel : in std_logic_vector(2 downto 0);
 		 ci : in std_logic;
 		 a : in std_logic_vector(4 downto 0);
 		 b : in std_logic_vector(4 downto 0);
@@ -23,7 +23,7 @@ package alu_devs is
 			 b: in std_logic_vector (3 downto 0);
 			 sa: in std_logic;
 			 sb: in std_logic;
-			 op_sel: in std_logic_vector (1 downto 0);
+			 op_sel: in std_logic_vector (2 downto 0);
 			 ci: in std_logic;
 			 co: out std_logic;
 			 sf: out std_logic;
@@ -33,7 +33,7 @@ package alu_devs is
 	-- Control de unidad aritmética
 	component ctrl_arith_u is
 		port( 
-		 c:in  std_logic_vector (1 downto 0);
+		 c:in  std_logic_vector (2 downto 0);
 		 ci:in  std_logic;
 		 a:in  std_logic_vector (3 downto 0);
 		 b:in  std_logic_vector (3 downto 0);
@@ -45,16 +45,16 @@ package alu_devs is
 	-- Unidad lógica
 	component logic_u is
 		port (
-		 a: in std_logic_vector (3 downto 0);
-		 b: in std_logic_vector (3 downto 0);
-		 op_sel: in std_logic_vector (1 downto 0);
+		 a: in std_logic_vector (4 downto 0);
+		 b: in std_logic_vector (4 downto 0);
+		 op_sel: in std_logic_vector (2 downto 0);
 		 ci: in std_logic;
-		 s: out std_logic_vector (3 downto 0)
+		 s: out std_logic_vector (4 downto 0)
 		);
 	end component;
 	-- Control de signo
 	component sign_control is
-		Port ( c_prime : in  STD_LOGIC_VECTOR (1 downto 0);
+		Port ( c_prime : in  STD_LOGIC_VECTOR (2 downto 0);
 			  ci_prime : in  STD_LOGIC;
            SAB : in  STD_LOGIC;
            SF : out  STD_LOGIC;
@@ -64,9 +64,9 @@ package alu_devs is
 	component operation_control is
 		Port ( SA : in  STD_LOGIC;
            SB : in  STD_LOGIC;
-           c : in  STD_LOGIC_VECTOR (1 downto 0);
+           c : in  STD_LOGIC_VECTOR (2 downto 0);
 			  ci : in STD_LOGIC;
-           c_prime : out  STD_LOGIC_VECTOR (1 downto 0);
+           c_prime : out  STD_LOGIC_VECTOR (2 downto 0);
 			  ci_prime : out STD_LOGIC;
            SAB : out  STD_LOGIC);
 	end component;
--- source/sims/alu_sim.vhd
+++ source/sims/alu_sim.vhd
@@ -6,46 +6,16 @@ entity alu_sim is
 end entity;
 
 architecture behavior of alu_sim is
-	signal unit_sel: std_logic;
-	signal op_sel: std_logic_vector(1 downto 0);
-	signal ci: std_logic;
-	signal a: std_logic_vector(3 downto 0);
-	signal b: std_logic_vector(3 downto 0);
-	signal sa: std_logic;
-	signal sb: std_logic;
+	signal instru: std_logic_vector(4 downto 0);
+	signal a: std_logic_vector(4 downto 0);
+	signal b: std_logic_vector(4 downto 0);
 	signal co: std_logic;
-	signal sf: std_logic;
-	signal s: std_logic_vector(3 downto 0);
+	signal s: std_logic_vector(4 downto 0);
 
 begin
 	
-	--Test B > A
-	
-	--SUM
-
-	unit_sel <= '0';
-	op_sel <= "00";
-	ci <= '0';
-	a <= "0101";
-	b <= "0110";
-	sa <= '0';
-	sb <= '0';
-	
-	alu_16op_block_sum_1: alu_16op port map(unit_sel,op_sel,ci,a,b,sa,sb,sf,co,s);
-	
-	sa <= '0';
-	sb <= '1';
-	
-	alu_16op_block_sum_2: alu_16op port map(unit_sel,op_sel,ci,a,b,sa,sb,sf,co,s);
-	
-	sa <= '1';
-	sb <= '0';
-	
-	alu_16op_block_sum_3: alu_16op port map(unit_sel,op_sel,ci,a,b,sa,sb,sf,co,s);
-
-	sa <= '1';
-	sb <= '1';
-	
-	alu_16op_block_sum_4: alu_16op port map(unit_sel,op_sel,ci,a,b,sa,sb,sf,co,s);
-		
+	a<="10101";
+	b<="00110" after 0 ns, "00001" after 6200 ps, "00010" after 6400 ps, "00011" after 6600 ps, "00100" after 6800 ps, "00001" after 7200 ps, "00010" after 7400 ps, "00011" after 7600 ps, "00100" after 7800 ps, "00001" after 8200 ps, "00010" after 8400 ps, "00011" after 8600 ps, "00100" after 8800 ps, "00101" after 8900 ps, "00001" after 9200 ps, "00010" after 9400 ps, "00011" after 9600 ps, "00100" after 9800 ps, "00101" after 9900 ps;
+	instru<="10000" after 0 ns, "10001" after 1 ns, "10010" after 2 ns, "10011" after 3 ns, "10100" after 4 ns, "10101" after 5 ns, "10110" after 6 ns, "10111" after 7 ns, "11000" after 8 ns, "11001" after 9 ns, "11010" after 10 ns;
+	alu_instancia: alu_16op port map (instru(4),instru(3 downto 1), instru(0), a, b, co, s);
 end architecture;
